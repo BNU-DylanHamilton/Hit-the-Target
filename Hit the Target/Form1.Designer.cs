9741f3916703063441f39e0bf9802c6d92b4c7e4
Dylan Hamilton <dylan5437@hotmail.co.uk>
2020-04-30 23:53:52 +0100

    Add project files.

---
+++ Hit the Target/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace Hit_the_Target
+{
+    partial class targetForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(targetForm));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.scoreLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.targetPictureBox = new System.Windows.Forms.PictureBox();
+            this.startButton = new System.Windows.Forms.Button();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.quitButton = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.targetPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.scoreLabel);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(719, 60);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(206, 203);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Results";
+            // 
+            // scoreLabel
+            // 
+            this.scoreLabel.AutoSize = true;
+            this.scoreLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scoreLabel.Location = new System.Drawing.Point(26, 107);
+            this.scoreLabel.Name = "scoreLabel";
+            this.scoreLabel.Size = new System.Drawing.Size(63, 20);
+            this.scoreLabel.TabIndex = 1;
+            this.scoreLabel.Text = "Score: ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(25, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Bulls Eye!";
+            // 
+            // targetPictureBox
+            // 
+            this.targetPictureBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.targetPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("targetPictureBox.Image")));
+            this.targetPictureBox.Location = new System.Drawing.Point(251, 108);
+            this.targetPictureBox.Name = "targetPictureBox";
+            this.targetPictureBox.Size = new System.Drawing.Size(100, 100);
+            this.targetPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.targetPictureBox.TabIndex = 1;
+            this.targetPictureBox.TabStop = false;
+            this.targetPictureBox.Click += new System.EventHandler(this.hitBullseye);
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(87, 477);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(123, 49);
+            this.startButton.TabIndex = 2;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startTimer);
+            // 
+            // stopButton
+            // 
+            this.stopButton.Location = new System.Drawing.Point(271, 477);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(123, 49);
+            this.stopButton.TabIndex = 3;
+            this.stopButton.Text = "Stop";
+            this.stopButton.UseVisualStyleBackColor = true;
+            this.stopButton.Click += new System.EventHandler(this.stopTimer);
+            // 
+            // quitButton
+            // 
+            this.quitButton.Location = new System.Drawing.Point(462, 477);
+            this.quitButton.Name = "quitButton";
+            this.quitButton.Size = new System.Drawing.Size(123, 49);
+            this.quitButton.TabIndex = 4;
+            this.quitButton.Text = "Quit";
+            this.quitButton.UseVisualStyleBackColor = true;
+            this.quitButton.Click += new System.EventHandler(this.closeApplication);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1250;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // targetForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(945, 546);
+            this.Controls.Add(this.quitButton);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.targetPictureBox);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "targetForm";
+            this.Text = "Hit the Target";
+            this.Click += new System.EventHandler(this.missedBullseye);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.targetPictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox targetPictureBox;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button stopButton;
+        private System.Windows.Forms.Button quitButton;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label scoreLabel;
+    }
+}
+
---
+++ Hit the Target/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hit_the_Target
+{
+    public partial class targetForm : Form
+    {
+        private int x, y, hitTarget;
+
+        private Random rand = new Random();
+
+        public targetForm()
+        {
+            InitializeComponent();
+        }
+        
+        /// <summary>
+        /// This method starts the timer with the normal monkey set in the 
+        /// picture box and sets the text of the labels.
+        /// </summary>
+        private void startTimer(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            scoreLabel.Text = "Score: " + Convert.ToString(hitTarget);
+        }
+
+        private void stopTimer(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+        }
+
+        private void hitBullseye(object sender, EventArgs e)
+        {
+            hitTarget++;
+            timer1.Enabled = false;
+            
+            //MessageBox.Show("You were " + Convert.ToString(Math.Sqrt(Math.Pow(targetPictureBox.Width - 50, 2) + Math.Pow(targetPictureBox.Height - 50, 2)) <= radius));
+        }
+
+        private void missedBullseye(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            MessageBox.Show("You missed the target.");
+        }
+
+        private void closeApplication(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        /// <summary>
+        /// This method sets up a timer that changes the position of the
+        /// monkey as the timers ticks on.
+        /// </summary>
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            x = rand.Next(Width - 100);
+            y = rand.Next(Height - 100);
+            targetPictureBox.Left = x;
+            targetPictureBox.Top = y;
+            Refresh();
+        }
+    }
+}
